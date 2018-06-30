c476329b6472b70830735e3d743b05164ea28cac
kenward27 <kenward27@gmail.com>
2018-06-30 16:25:41 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+
+
+// npm start robofriends
+ReactDOM.render(<App />,
+	document.getElementById('root'));
+registerServiceWorker();
